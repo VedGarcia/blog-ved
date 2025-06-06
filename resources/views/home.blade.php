936c87f0ca8f378889f12d5d673c8b84f3b146fe
Victor SUSCERTE <garciaveduar@gmail.com>
2025-06-06 16:22:15 -0400

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('template')
+@section('content')
+<h1>This is Home</h1>
+    
+@endsection
---
+++ resources/views/post.blade.php
@@ -0,0 +1,8 @@
+@extends('template')
+@section('content')
+    
+<h1>Detalles</h1>
+
+{{$post}}
+
+@endsection
