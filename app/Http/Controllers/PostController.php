32f35d45691a1e51752b6cb81a29ffe62e9df3cd
Victor SUSCERTE <garciaveduar@gmail.com>
2025-06-16 16:39:49 -0400

    Improves post creation and update logic
    Adds validation rules to ensure required fields are present when creating and updating posts.
    
    It also corrects the slug generation and uses the `compact` function for passing data to views, making the code cleaner and more readable.
    
    Additionally, it preserves old input values when validation fails, improving user experience.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 
-use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class PostController extends Controller
 {
@@ -18,27 +18,44 @@ class PostController extends Controller
     
     public function create(Post $post)
     {
-        return view('posts.create', ['post' => $post]);
+        return view('posts.create', compact('post'));
     } 
     
      public function store(Request $request)
     {
+        $request->validate([
+            'title' => 'required',
+            'slug' => 'required|unique:posts,slug',
+            'body' => 'required'
+        ]);
+
         $post = $request->user()->posts()->create([
-            'title' => $title = $request->title,
-            'slug' => Str::slug('title'),
-            'body' => $request->body
+            'title' => $request->title,
+            'slug' =>  $request->slug,
+            'body' =>  $request->body
         ]);
         return redirect()->route('posts.edit', $post);
     } 
 
     public function edit(Post $post)
     {
-        return view('posts.edit', ['post' => $post]);
+        return view('posts.edit', compact('post'));
     }
 
-    public function update(Post $post)
+    public function update(Request $request, Post $post) 
     {
-        return view('posts.edit', ['posts' => $post]);
+        $request->validate([
+            'title' =>  'required',
+            'slug' =>   'required|unique:posts,slug,' . $post->id,
+            'body' =>   'required'
+        ]);
+        
+         $post->update([
+            'title' => $request->title,
+            'slug' =>  $request->slug,
+            'body' =>  $request->body
+        ]);
+        return redirect()->route('posts.edit', $post);
     }
 
      public function destroy(Post $post)
--- resources/views/posts/_form.blade.php
+++ resources/views/posts/_form.blade.php
@@ -1,10 +1,31 @@
 @csrf
 
 <label for="" class="uppercase dark:text-gray-200 text-xs">Titulo</label>
-<input type="text" name="title" class="rounded bg-stone-800 border-gray-800 w-full mb-4 text-gray-800" value="{{$post->title}}">
+<span class="text-xs text-red-600">
+    @error('title') 
+    {{$message}}
+    @enderror
+</span>
+<input type="text" name="title" class="rounded bg-stone-800 border-gray-800 w-full mb-4 text-gray-800" value="{{ old('title', $post->title)}}">
+
+
+<label for="" class="uppercase dark:text-gray-200 text-xs">Slug</label>
+<span class="text-xs text-red-600">
+    @error('slug') {{$message}}
+        
+    @enderror
+</span>
+
+<input type="text" name="slug" class="rounded bg-stone-800 border-gray-800 w-full mb-4 text-gray-800" value="{{old('slug', $post->slug)}}">
+
 
 <label for="" class="uppercase dark:text-gray-200 text-xs">Contenido</label>
-<textarea name="" rows="5" class="rounded bg-stone-800 border-gray-800 w-full mb-4 text-gray-800">{{$post->body}}</textarea>
+<span class="text-xs text-red-600">
+    @error('body') 
+    {{$message}}
+    @enderror
+</span>
+<textarea name="body" rows="5" class="rounded bg-stone-800 border-gray-800 w-full mb-4 text-gray-800">{{ old('body', $post->body)}}</textarea>
 
 <div class="flex justify-between items-center">
     <a href="{{route('posts.index')}}" class="dark:text-indigo-300">Volver</a>
